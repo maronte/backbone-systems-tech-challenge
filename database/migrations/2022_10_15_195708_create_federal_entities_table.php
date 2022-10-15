3bed840fca442fe9ec99e64b1c3e3d5707e167f4
Ronaldo Trejo <ma.ronaldotrejo@gmail.com>
2022-10-15 15:40:44 -0500

    feat: add migration for "federal entities" table

---
+++ database/migrations/2022_10_15_195708_create_federal_entities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('federal_entities', function (Blueprint $table) {
+            $table->tinyInteger(column: 'id', unsigned: true);
+            $table->string(column: 'name', length: 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('federal_entities');
+    }
+};
